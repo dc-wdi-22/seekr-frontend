79cc76ce8edf773487dc4d36d8ef3efdff12ff68
Peterson Onyeukwu <code.jrgroup@gmail.com>
2018-05-08 16:17:44 -0400

    create job rows

--- seekr-app/src/components/JobRows.js
+++ seekr-app/src/components/JobRows.js
@@ -1,66 +1,39 @@
 import React,{Component} from 'react'
 
-const fakeData = [
-  {
-    title: "Coder",
-    company: "General Assembly",
-    location: "Washington DC",
-    url: "https://www.google.com",
-    closing_date: "May 15, 2018",
-    requirements: "N/A",
-    salary_range: "50,000-75,000",
-    pt_of_contact: "James Superbuggy",
-    source: "google.com"
-  },
-  {
-    title: "Specialist",
-    company: "Apple",
-    location: "Cupertino, CA",
-    url: "https://www.apple.com/",
-    closing_date: "May 8, 2018",
-    requirements: "N/A",
-    salary_range: "80,000-100,000",
-    pt_of_contact: "Ali Spittel",
-    source: "apple.com"
-  },
-  {
-    title: "Coder2",
-    company: "GW Bootcamp",
-    location: "Washington DC",
-    url: "www.gwu.edu",
-    closing_date: "May 17, 2018",
-    requirements: "N/A",
-    salary_range: "600,000-650,000",
-    pt_of_contact: "Joy Outcomes",
-    source: "www.google.com"
-  }
-]
-
 class JobRows extends Component {
-  constructor () {
+  constructor(){
     super()
     this.state = {
-      fakeData: []
+      rows: []
     }
   }
-  componentDidMount () {
-    // waiting on api work from jason and matt
-    this.setState({fakeData: fakeData})
-
-  }
-  render(){
-    const rows = this.props.jobs.map( (job,i) => {
-      return (
-      <div key={i}>
-        <h3>{job.title}</h3>
-        <h3>{job.company}</h3>
-      </div>
-      )
+  componentDidMount(){
+    console.log('testing', this.props.filter)
+    let filteredRows = this.props.jobs.filter( (job) => {      
+      console.log(this.props.filter)
+      return job.job_status === this.props.filter
     })
+
+    this.setState({rows: filteredRows })
+ }
+
+  render () {
+    let renderedRows = this.state.rows.map( (job,i) => {
+      return (
+        <li className="job" key={i}>
+          <p>{job.company.name}</p>
+          <p>{job.title}</p>
+          <p>{job.job_status}</p>
+        </li>
+      )
+   })
     return(
       <div>
-        <h1>Job Component</h1>
-        { rows }
+        <div className="job-row">
+          <ul className="job-container">
+          { renderedRows }
+          </ul>
+        </div>
       </div>
     )
   }
