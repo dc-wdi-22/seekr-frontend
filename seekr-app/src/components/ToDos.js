94cf283aadbefe09718cbff6995547e1dd11054b
Jason Toups <jason.p.toups@gmail.com>
2018-05-09 12:06:02 -0400

    to do checkbox sends put request to api

--- seekr-app/src/components/ToDos.js
+++ seekr-app/src/components/ToDos.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class ToDo extends Component {
   constructor (props) {
-    super()
+    super(props)
+    this.state = {
+      status: this.props.item.status,
+      name: this.props.item.name
+    }
     this.handleChange = this.handleChange.bind(this)
   }
 
-  handleChange () {
-    console.log('checkbox clicked')
+  handleChange (e) {
+    let newStatus = e.target.checked
+    this.setState({
+      status: newStatus
+    })
+  }
+
+  componentDidUpdate () {
+    axios
+      .put(`http://localhost:8000/todos/${this.props.item.pk}`, {
+        status: this.state.status,
+        name: this.state.name
+      })
+      .catch(err => console.log(err))
   }
 
   render () {
     return (
       <div>
-        <input type='checkbox' value={this.props.item.status} name={this.props.item.name} onChange={this.handleChange} />
+        <input type='checkbox' checked={this.state.status} name={this.state.name} onChange={this.handleChange} />
         <label>{this.props.item.name}</label>
       </div>
     )
