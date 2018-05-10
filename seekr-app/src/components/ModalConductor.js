44d3d8a052eb0f22105f0bf0794270b3a46dbe01
Matthew Sharpe <mattsharpe72@gmail.com>
2018-05-10 12:11:20 -0400

    addjob working

--- seekr-app/src/components/ModalConductor.js
+++ seekr-app/src/components/ModalConductor.js
@@ -3,36 +3,36 @@ import JobDetails from './JobDetails'
 import NewJob from './NewJob'
 import Main from './Main'
 
-const example = [{
-  'pk': 1,
-  'company': {
-    'pk': 1,
-    'name': 'Apple',
-    'industry': 'Information Technology',
-    'address': 'Cupertino, CA',
-    'url': 'https://www.apple.com/',
-    'glassdoor_link': 'https://www.glassdoor.com/Overview/Working-at-Apple-EI_IE1138.11,16.htm',
-    'jobs': [
-      'http://localhost:8000/job/2',
-      'http://localhost:8000/job/1'
-    ]
-  },
-  'title': 'Technical Specialist',
-  'description': 'You help new owners get started and current ones get quick, efficient support — developing strong, positive relationships with Apple. ',
-  'requirements': 'Ability to assess customers\\’ support needs when they arrive, then provide solutions or refer them to other team members. ',
-  'salary_range_start': 36000,
-  'salary_range_end': 50000,
-  'source': 'glassdoor',
-  'notes': 'yes',
-  'date_posted': '2018-02-12',
-  'todo_list': [
-    {
-      'status': false,
-      'name': 'Set up app',
-      'pk': 1
-    }],
-  'job_status': 'Applied'
-}]
+// const example = [{
+//   'pk': 1,
+//   'company': {
+//     'pk': 1,
+//     'name': 'Apple',
+//     'industry': 'Information Technology',
+//     'address': 'Cupertino, CA',
+//     'url': 'https://www.apple.com/',
+//     'glassdoor_link': 'https://www.glassdoor.com/Overview/Working-at-Apple-EI_IE1138.11,16.htm',
+//     'jobs': [
+//       'http://localhost:8000/job/2',
+//       'http://localhost:8000/job/1'
+//     ]
+//   },
+//   'title': 'Technical Specialist',
+//   'description': 'You help new owners get started and current ones get quick, efficient support — developing strong, positive relationships with Apple. ',
+//   'requirements': 'Ability to assess customers\\’ support needs when they arrive, then provide solutions or refer them to other team members. ',
+//   'salary_range_start': 36000,
+//   'salary_range_end': 50000,
+//   'source': 'glassdoor',
+//   'notes': 'yes',
+//   'date_posted': '2018-02-12',
+//   'todo_list': [
+//     {
+//       'status': false,
+//       'name': 'Set up app',
+//       'pk': 1
+//     }],
+//   'job_status': 'Applied'
+// }]
 
 class ModalConductor extends Component {
   constructor (props) {
@@ -76,7 +76,7 @@ class ModalConductor extends Component {
         <button onClick={this.openJobDetails}>Job Details</button>
         {this.state.jobDetailsModal && <JobDetails isOpen={this.state.jobDetailsModal} onRequestClose={this.closeJobDetails} job={this.state.targetJob} />}
 
-        <button onClick={this.openNewJob}>Add Job</button>
+        {/* <button onClick={this.openNewJob}>Add Job</button> */}
         {this.state.newJobModal && <NewJob isOpen={this.state.newJobModal} onRequestClose={this.closeNewJob} />}
       </div>
 
