d8245a6ad04448d46f001978ca53c907351f5deb
Meron Kebede <kebedemeron905@gmail.com>
2018-05-11 10:08:24 -0400

    add new company

--- seekr-app/src/components/ModalConductor.js
+++ seekr-app/src/components/ModalConductor.js
@@ -4,6 +4,7 @@ import NewJob from './NewJob'
 import Main from './Main'
 import Axios from 'axios'
 import {CLIENT_URL} from '../constants.js'
+import NewCompany from './NewCompany'
 
 class ModalConductor extends Component {
   constructor (props) {
@@ -13,15 +14,19 @@ class ModalConductor extends Component {
       currentJobData: {},
       targetJob: {},
       newJobModal: false,
+      newCompanyModal: false,
       todos: [],
       jobs: [],
       companies: []
+
     }
     this.openJobDetails = this.openJobDetails.bind(this)
     this.closeJobDetails = this.closeJobDetails.bind(this)
     this.openNewJob = this.openNewJob.bind(this)
     this.closeNewJob = this.closeNewJob.bind(this)
     this.updatePage = this.updatePage.bind(this)
+    this.openNewCompany = this.openNewCompany.bind(this)
+    this.closeNewCompany = this.closeNewCompany.bind(this)
   }
   openJobDetails (target, event) {
     this.setState({
@@ -31,7 +36,10 @@ class ModalConductor extends Component {
   }
 
   closeJobDetails () {
-    this.setState({jobDetailsModal: false})
+    this.setState({
+      jobDetailsModal: false,
+      targetJob: {}
+    })
   }
 
   openNewJob () {
@@ -46,6 +54,14 @@ class ModalConductor extends Component {
       newJobModal: false,
       targetJob: {}
     })
+  }
+
+  openNewCompany () {
+    this.setState({newCompanyModal: true})
+  }
+
+  closeNewCompany () {
+    this.setState({newCompanyModal: false})
   }
 
   updatePage () {
@@ -73,11 +89,13 @@ class ModalConductor extends Component {
     console.log('Modal Conductor rendering', this.state)
     return (
       <div>
-        <Main openJobDetails={this.openJobDetails} openNewJob={this.openNewJob} jobs={this.state.jobs} companies={this.state.companies} />
+        <Main openJobDetails={this.openJobDetails} openNewJob={this.openNewJob} openNewCompany={this.openNewCompany} jobs={this.state.jobs} companies={this.state.companies} />
 
         {this.state.jobDetailsModal && <JobDetails isOpen={this.state.jobDetailsModal} openNewJob={this.openNewJob} onRequestClose={this.closeJobDetails} job={this.state.targetJob} todos={this.state.todos} updatePage={this.updatePage} />}
 
         {this.state.newJobModal && <NewJob isOpen={this.state.newJobModal} onRequestClose={this.closeNewJob} job={this.state.targetJob} updatePage={this.updatePage} />}
+
+        {this.state.newCompanyModal && <NewCompany isOpen={this.state.newCompanyModal} onRequestClose={this.closeNewCompany} updatePage={this.updatePage} />}
 
       </div>
 
